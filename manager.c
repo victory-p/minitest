a4f64cf73e8cfe26c24eb40e95133ee07bcfe893
Park Seungri <21900282@handong.edu>
2020-04-27 23:33:34 +0900

    Update manager.c

--- manager.c
+++ manager.c
@@ -40,7 +40,11 @@ void saveData(Product p[], int count){
 
 	//중량 가격 제품명
 	fp= fopen("product.txt","wt");
-	
+	for(int i=0; i<count; i++){
+		if(p[i].weight != -1 || p[i].price != -1){
+		fprintf(fp, "%d %d %s\n", p[i].weight, p[i].price, p[i].name);
+		}
+	}
 	
 	fclose(fp);
 	printf("저장됨!\n");
